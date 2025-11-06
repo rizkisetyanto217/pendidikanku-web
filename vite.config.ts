bb50f514084d5477509807690342bf238a2abd2f
Rizki Setyanto <rizkisetyanto217@gmail.com>
2025-11-06 17:05:01 +0700

    fix vite config (1Ac)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    // pakai HTTP saja (tanpa https)
+    port: 5174,
+    proxy: {
+      "/api": {
+        target: "https://masjidkubackend4-production.up.railway.app",
+        changeOrigin: true,
+        secure: true,
+        // backend kamu memang pakai prefix /api, jadi TIDAK perlu rewrite
+        // rewrite: (p) => p.replace(/^\/api/, "/api"),
+
+        // ini agar Set-Cookie dari Railway disimpan sebagai cookie 'localhost'
+        cookieDomainRewrite: "localhost",
+      },
     },
   },
 });
